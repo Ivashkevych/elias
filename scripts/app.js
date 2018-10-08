0ab8ca19d4c2dd450f525f9f396fe9eb278a8160
Ivashkevych Andrij <ivashkevich_andrei@bigmir.net>
2018-10-08 13:57:05 +0300

    Finished.

---
+++ scripts/app.js
@@ -0,0 +1,18 @@
+/**
+ * Created by szhyvotov on 12/10/14.
+ */
+
+(function (){
+    'use strict';
+
+    $(document).ready(function(){
+
+        $("#slider").owlCarousel({
+            autoPlay: true,
+            singleItem: true,
+            transitionStyle : "fade"
+        });
+
+    });
+
+}());
